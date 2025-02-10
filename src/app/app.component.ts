63958124e0f893502438b33b602f56688e81f77d
Abhisriganta <gantaabhi1173@gmail.com>
2025-02-10 22:59:48 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'weather';
+  city:string=''
+  dcity:String=''
+  temp_c:string=''
+  wind:string=''
+  humidity:string=''
+  image:string=''
+
+  setCity(event:any){
+      this.city=event.target.value
+  }
+  constructor(private weather:WeatherService){}
+
+  checkWeather(){
+    this.weather.fetchWeather(this.city)
+    .then(data=>{
+      this.dcity=this.city
+      this.temp_c="Temperature: " +data.temp_c
+      this.wind="Wind Speed" + data.wind +" km/hr"
+      this.humidity="Humidity: "+data.humidity
+      this.image = data.condition.icon
+    })
+  }
+
 }
